eff0d1d437ca6b164aeb94b8f74233f25750cef6
shuaibcs <alshuaibi7@gmail.com>
2019-09-15 18:56:28 +0200

    Values JSX Can't Show

--- src/index.js
+++ src/index.js
@@ -9,18 +9,25 @@ import ReactDOM from 'react-dom'
 
 // Create a react component
 const App = function() {
-    const buttonText = 'Click Me!'
+    // const buttonText = 12345 or ['Hi', 'There'] or [10, 20]
+    // const buttonText = 'Click Me!'
+    // const style={backgroundColor: 'blue', color: 'white' }
+    const buttonText = { text: 'Click Me!' }
+    const labelText = 'Enter name:'
     return (  
     <div>
         {/* instead of class */}
         <label className="label" for="name"> 
-            Enter name:
+            {/* Enter name: */}
+            {labelText}
         </label>
         <input id="name" type="text" />
+        {/* <button style={style}> */}
         {/* <button style="background-color: blue; color: white">Submit</button> */}
         <button style={{backgroundColor: 'blue', color: 'white' }}>
             {/* Submit */}
-            {buttonText}
+            {/* {buttonText} */}
+            {buttonText.text}   
             {/* {getButtonText()} */}
         </button>
     </div>
